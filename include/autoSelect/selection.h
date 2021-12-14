b628978ba84fa7d60d99aa62204b36429ce39487
TerryKuuuu <68409225+TerryKuuuu@users.noreply.github.com>
2021-12-14 18:37:30 +0800

    Add files via upload

---
+++ include/autoSelect/selection.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+//selector configuration
+#define HUE 360
+#define DEFAULT 2
+#define AUTONS "Left", "Right", "Do Nth"
+
+namespace selector{
+
+extern int auton;
+const char *b[] = {AUTONS, ""};
+void init(int hue = HUE, int default_auton = DEFAULT, const char **autons = b);
+
+}
